6d095d1708394d15c791d1714d7edc58c7b42535
apandey <apandey@gap.com>
2020-11-21 11:55:18 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'translation-app';
+  variable = "Nothing";
+  title = 'Simple Translate';
+  doSomething(val){
+    document.getElementById("lblName").innerHTML = val;
+    //console.log("Hello World");
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+              + (<HTMLInputElement>document.getElementById("sl")).value + "&tl=" + (<HTMLInputElement>document.getElementById("tl")).value + "&dt=t&q=" + encodeURI(val);
+    //console.log(url);
+    fetch(url,{method: 'POST'})
+    .then(res => res.json())
+    .then(data => {
+      //console.log(data);
+      //console.log(data[0][0][0]);
+      document.getElementById("lblName").innerHTML = data[0][0][0];
+    })
+    .catch(error => console.log('Error'));
+    //console.log(res);
+
+    /*
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    console.log(xhr.send());
+    */
+  }
 }
